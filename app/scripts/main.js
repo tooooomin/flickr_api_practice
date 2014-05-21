7432b0c0bf0ee1cf3a12f6de0a9fd16bded26851
Tomoko Suzuki <suzuki_tomoko@cyberagent.co.jp>
2014-05-21 19:21:59 +0900

    flicker prj

---
+++ app/scripts/main.js
@@ -0,0 +1,12 @@
+/*
+ * Flicker
+ *
+ * Copyright (c) 2014 Tomoko Suzuki
+ */
+(function($, window, document) {
+    $(function() {
+        // The DOM is ready
+    });
+    
+    // The rest of the code goes here!
+}(window.jQuery, window, document));
